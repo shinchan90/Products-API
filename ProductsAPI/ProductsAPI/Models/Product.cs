7709b7f7a21131ba2ab7e70dcc4cb423522656e0
Ema <emajacinto061@gmail.com>
2023-05-31 15:14:46 +0100

    update

--- ProductsAPI/ProductsAPI/Models/Product.cs
+++ ProductsAPI/ProductsAPI/Models/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,7 +9,10 @@ namespace ProductsAPI.Models
     public class Product
     {
         public int Id { get; set; }
+        
+        [MaxLength(30)]
         public string Name { get; set; }
+        [MaxLength(300)]
         public string Description { get; set; }
         public float Price { get; set; }
         public int Quantity { get; set; }
